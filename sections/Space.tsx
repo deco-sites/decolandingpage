5ed9e17219b9350a01874f2abe2f611a0e2d6027
Kesler <kesler.m.pereira@gmail.com>
2023-06-19 19:50:33 -0300

    feat before gsap

--- sections/Space.tsx
+++ sections/Space.tsx
@@ -1,22 +1,23 @@
 export interface props {
-    spaceText: string;
-    description: string[];
-  }
-  
-  export default function Header({ spaceText }: props,
-    
-  ) {
-    return (
-        <div>
-            <script>
-                {`
+  spaceText: string;
+  description: string[];
+}
+
+export default function Header({ spaceText }: props) {
+  return (
+    <div>
+      <script>
+        {`
                     
                 `}
-            </script>
-            <div className={'flex items-center flex-col'} id="space">
-                <h2 className={'text-white text-[32px] mb-[20px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[165px]'}>{spaceText}</h2>
-            </div>
-        </div>
-    );
-  }
-  
+      </script>
+      <div className={"flex items-center flex-col"} id="space">
+        <h2
+          className={"text-white text-[32px] mb-[20px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[165px]"}
+        >
+          {spaceText}
+        </h2>
+      </div>
+    </div>
+  );
+}
