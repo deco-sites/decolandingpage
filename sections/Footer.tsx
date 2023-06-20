47023562bfc453d823c921eb777f2363a9c50e59
Kesler <kesler.m.pereira@gmail.com>
2023-06-20 10:38:04 -0300

    feat: madeby

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,18 +1,17 @@
 export interface props {
-    spaceText: string;
-  }
-  
-  export default function Space({ spaceText }: props) {
-    return (
-      <div>
-        <div className={"flex items-center flex-col"} id="space">
-          <h2
-            className={"text-white text-[32px] mb-[50px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[165px] lg:mb-[165px]"}
-            dangerouslySetInnerHTML={{ __html: spaceText }}
-          >
-          </h2>
-        </div>
+  spaceText: string;
+}
+
+export default function Space({ spaceText }: props) {
+  return (
+    <div>
+      <div className={"flex items-center flex-col"} id="space">
+        <h2
+          className={"text-white text-[32px] mb-[50px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[165px] lg:mb-[165px]"}
+          dangerouslySetInnerHTML={{ __html: spaceText }}
+        >
+        </h2>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- sections/MadeBy.tsx
+++ sections/MadeBy.tsx
@@ -1,36 +1,44 @@
 export interface props {
-    text: string;
-    devsCircle: {
-        image: string;
-        name: string;
-        link: string;
-    }[];
-  }
-  
-  export default function MadeBy({ text, devsCircle }: props) {
-    return (
-      <div>
-        <div className={"flex items-center flex-col"} id="space">
-          <h2
-            className={"text-white text-[32px] mb-[50px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[165px] lg:mb-[165px]"}
-            dangerouslySetInnerHTML={{ __html: text }}
-          >
-          </h2>
-        </div>
+  text: string;
+  devsCircle: {
+    image: string;
+    name: string;
+    link: string;
+  }[];
+}
 
-        <div className={"flex flex-col items-center justify-center"}>
-            <div className={"flex flex-wrap justify-center"}>
-                {devsCircle.map((dev, index) => (
-                    <div key={index} className={"flex flex-col items-center justify-center m-[20px]"}>
-                        <a href={dev.link} target="_blank" rel="noopener noreferrer">
-                            <img src={dev.image} alt={dev.name} className={"w-[100px] h-[100px] rounded-full"} />
-                        </a>
-                        <p className={"text-white text-[16px] font-semibold mt-[10px]"}>{dev.name}</p>
-                    </div>
-                ))}
+export default function MadeBy({ text, devsCircle }: props) {
+  return (
+    <div>
+      <div className={"flex items-center flex-col"} id="space">
+        <h2
+          className={"text-white text-[32px] mb-[50px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[165px] lg:mb-[165px]"}
+          dangerouslySetInnerHTML={{ __html: text }}
+        >
+        </h2>
+      </div>
+
+      <div className={"flex flex-col items-center justify-center"}>
+        <div className={"flex flex-wrap justify-center"}>
+          {devsCircle.map((dev, index) => (
+            <div
+              key={index}
+              className={"flex flex-col items-center justify-center m-[20px]"}
+            >
+              <a href={dev.link} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={dev.image}
+                  alt={dev.name}
+                  className={"w-[100px] h-[100px] rounded-full"}
+                />
+              </a>
+              <p className={"text-white text-[16px] font-semibold mt-[10px]"}>
+                {dev.name}
+              </p>
             </div>
+          ))}
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
