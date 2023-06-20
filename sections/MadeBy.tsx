c1f4f2556c3321675a4b4a8d2f2b5b217338eaf6
Kesler <kesler.m.pereira@gmail.com>
2023-06-20 16:15:12 -0300

    feat:

--- sections/MadeBy.tsx
+++ sections/MadeBy.tsx
@@ -9,7 +9,7 @@ export interface props {
 
 export default function MadeBy({ text, devsCircle }: props) {
   return (
-    <div>
+    <div id="MadeBy">
       <div className={"flex items-center flex-col"} id="space">
         <h2
           className={"text-white text-[32px] mb-[50px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[100px] lg:mb-[100px] flex-col flex"}
@@ -22,7 +22,7 @@ export default function MadeBy({ text, devsCircle }: props) {
         className={"flex flex-col items-center justify-center w-[100vw] lg:w-[auto] overflow-auto"}
       >
         <div
-          className={"flex w-[100vw] lg:w-[auto] lg:flex-wrap lg:justify-center lg:gap-[80px]"}
+          className={"flex w-[100vw] lg:w-[auto] lg:flex-wrap lg:justify-center lg:gap-x-[100px] gap-y-[20px] mb-[45px] lg:mb-[120px]"}
         >
           {devsCircle.map((dev, index) => (
             <div
@@ -33,10 +33,10 @@ export default function MadeBy({ text, devsCircle }: props) {
                 <img
                   src={dev.image}
                   alt={dev.name}
-                  className={"w-[100px] h-[100px] min-w-[100px] rounded-full lg:w-[150px] lg:h-[150px] lg:min-w-[150px]"}
+                  className={"w-[76px] min-w-[76px] h-[76px] rounded-full lg:min-w-[95px] lg:min-h-[95px] lg:w-[95px]"}
                 />
               </a>
-              <p className={"text-white text-[16px] font-semibold mt-[10px]"}>
+              <p className={"text-white text-[16px] font-[400] mt-[20px]"}>
                 {dev.name}
               </p>
             </div>
