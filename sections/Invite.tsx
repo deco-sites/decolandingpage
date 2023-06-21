fb7e0bf932130215e0c348c55b26a26a721dd374
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-06-21 14:10:10 -0300

    Improve data room button

--- sections/Invite.tsx
+++ sections/Invite.tsx
@@ -28,11 +28,14 @@ export default function Invite(
           href={buttonLink}
           target="_blank"
           rel="noopener noreferrer"
-          className={"flex items-center justify-center text-[20px] font-[500] px-[46px] py-[24px] text-[#f3fff9] bg-[#0A2121] rounded-[8px] my-[40px] lg:text-[48px] lg:font-[400] lg:px-[64px] lg:py-[30px] lg:mt-[76px] lg:mb-[80px]"}
+          class="flex items-center justify-center text-[20px] font-[500] px-[46px] py-[24px] text-[#f3fff9] bg-[#0A2121] rounded-[8px] my-[40px] lg:text-[48px] lg:font-[400] lg:px-[64px] lg:py-[30px] lg:mt-[76px] lg:mb-[80px] lg:hover:scale-110 duration-150 overflow-hidden group relative   hover:bg-gradient-to-r hover:from-[#0A2121] hover:to-gray-800"
+
         >
-          {buttonText}
+          <span class="absolute -right-10 w-20 h-60 -mt-12 transition-all duration-500 transform translate-x-12 bg-white opacity-5 rotate-12 group-hover:-translate-x-[800px] ease"></span>
+          <span class="relative">{buttonText}</span>
         </a>
       </div>
     </div>
   );
 }
+
