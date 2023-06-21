c09fff562636e86df38c81d0534c2b44f5befc8e
Kesler <kesler.m.pereira@gmail.com>
2023-06-21 09:53:21 -0300

    f

--- islands/Stars.tsx
+++ islands/Stars.tsx
@@ -4,56 +4,57 @@ import { useEffect } from "preact/hooks";
 const Stars = () => {
   useEffect(() => {
     const space = document.getElementById(`space`) as HTMLElement,
-    spaceWidth = space.scrollWidth,
-    spaceHeight = space.scrollHeight,
-    perspective = 100;
-const frag = document.createDocumentFragment();
+      spaceWidth = space.scrollWidth,
+      spaceHeight = space.scrollHeight,
+      perspective = 100;
+    const frag = document.createDocumentFragment();
 
-space.style.setProperty(`--perspective`, `${perspective}px`)
+    space.style.setProperty(`--perspective`, `${perspective}px`);
 
-function makeStar() {
-  const star = document.createElement(`time`),
+    function makeStar() {
+      const star = document.createElement(`time`),
         starWidth = gsap.utils.random(0.7, 1.1, 1),
         starHeight = starWidth * gsap.utils.random(10, 20, 1),
         randomRotation = Math.random() * 360,
-        scaleModifier = Math.random()
+        scaleModifier = Math.random();
 
-  const visibleRangeMaximum = (spaceWidth - spaceHeight > 0 ? spaceWidth : spaceHeight) / 1.5
+      const visibleRangeMaximum =
+        (spaceWidth - spaceHeight > 0 ? spaceWidth : spaceHeight) / 1.5;
 
-  gsap.set(star, {
-    width: `${starWidth}px`,
-    height: `${starHeight}px`,
-    backgroundColor: "#06E474",
-    transform: `
+      gsap.set(star, {
+        width: `${starWidth}px`,
+        height: `${starHeight}px`,
+        backgroundColor: "#06E474",
+        transform: `
       translateY(${starHeight / 2}px)
       rotate(${randomRotation}deg)
       rotateX(90deg)
       translateZ(0px)
       scaleX(${scaleModifier})
     `,
-  });
+      });
 
-  gsap.to(star, {
-    duration: "random(10, 20)",
-    transform: `
+      gsap.to(star, {
+        duration: "random(10, 20)",
+        transform: `
       translateY(${starHeight / 2}px)
       rotate(${randomRotation}deg)
       rotateX(90deg)
       translateZ(${perspective + visibleRangeMaximum}px)
       scaleX(${scaleModifier})
     `,
-    repeat: -1,
-    ease: `none`,
-  }).progress( Math.random() );
+        repeat: -1,
+        ease: `none`,
+      }).progress(Math.random());
 
-  frag.appendChild(star);
-  }
+      frag.appendChild(star);
+    }
 
-  for (let i = 0; i < 200; i++) {
-  makeStar();
-  }
+    for (let i = 0; i < 200; i++) {
+      makeStar();
+    }
 
-  space.appendChild(frag);
+    space.appendChild(frag);
   }, []);
 
   return (null);
--- sections/LoseSales.tsx
+++ sections/LoseSales.tsx
@@ -4,9 +4,9 @@ import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 export interface props {
   principalText: string;
   bullets?: {
-    icon?: LiveImage;
+    icon: LiveImage;
     text?: string;
-  }[]
+  }[];
 }
 
 export default function LoseSales({ principalText, bullets }: props) {
@@ -18,7 +18,9 @@ export default function LoseSales({ principalText, bullets }: props) {
           "linear-gradient(180deg, rgba(94, 61, 232, 0.2) 0%, rgba(247, 25, 99, 0.2) 100%)",
       }}
     >
-      <div className={"flex items-center flex-col gap-8 lg:gap-16 mx-auto w-[90%]"}>
+      <div
+        className={"flex items-center flex-col gap-8 lg:gap-16 mx-auto w-[90%]"}
+      >
         <h2
           className={"text-white text-[32px] mb-[20px] lg:mb-[40px] font-semibold lg:text-[73px] text-center mt-[50px] lg:mt-[154px] leading-[125%]"}
         >
@@ -28,9 +30,12 @@ export default function LoseSales({ principalText, bullets }: props) {
           className={"text-center flex flex-col lg:flex-row lg:flex lg:mb-[165px] gap-12 mb-[40px]"}
         >
           {bullets?.map((item, index) => (
-            <div key={index} class="flex flex-col items-center gap-4 text-[#F3FFF9] text-[22px] text-center lg:text-[40px] max-w-[300px]">
+            <div
+              key={index}
+              class="flex flex-col items-center gap-4 text-[#F3FFF9] text-[22px] text-center lg:text-[40px] max-w-[300px]"
+            >
               <Image
-                src={item?.icon}
+                src={item.icon}
                 alt={item?.text}
                 width={60}
                 height={60}
