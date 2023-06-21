4567649c8da6c2d9c0c8c0b8ad35fddb32467ba6
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-06-21 17:45:32 -0300

    Improve team

--- sections/MadeBy.tsx
+++ sections/MadeBy.tsx
@@ -24,31 +24,26 @@ export default function MadeBy({ text, imageSize, devsCircle }: props) {
       </div>
 
       <div
-        className={"flex flex-col items-center justify-center w-[100vw] lg:w-[auto] overflow-auto"}
+        className={"flex flex-col items-center justify-center w-[100vw] lg:w-[auto] overflow-auto lg:overflow-visible"}
       >
         <div
           className={"flex w-[100vw] lg:w-[auto] lg:flex-wrap lg:justify-center gap-y-10 mb-[45px] lg:mb-[120px]"}
         >
           {devsCircle.map((dev, index) => (
-            <div
-              key={index}
-              className={"flex flex-col items-center px-4 w-44 gap-4 text-center"}
-            >
-              <a href={dev.linkedin} target="_blank" rel="noopener noreferrer">
-                <Image
-                  src={dev.image || "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1493/ff1f2e56-d896-484b-b3bf-f411c0df4c3b"}
-                  alt={dev.label}
-                  className={"rounded-full min-w-[98px]"}
-                  width={imageSize}
-                  height={imageSize}
-                />
-              </a>
+            <a key={index} href={dev.linkedin} target="_blank" rel="noopener noreferrer" class="flex flex-col items-center px-4 w-44 gap-4 text-center group">
+              <Image
+                src={dev.image || "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1493/ff1f2e56-d896-484b-b3bf-f411c0df4c3b"}
+                alt={dev.label}
+                class="rounded-full min-w-[98px] group-hover:scale-110 duration-200"
+                width={imageSize}
+                height={imageSize}
+              />
               <p
                 className={"text-white text-[16px] font-[400]"}
               >
                 {dev.label}
               </p>
-            </div>
+            </a>
           ))}
         </div>
       </div>
